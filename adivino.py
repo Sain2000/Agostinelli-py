73d8fc0852710225a27fea6b0167a531ce6c0843
Sain2000 <86694778+Sain2000@users.noreply.github.com>
2022-03-23 18:36:00 -0300

    Update adivino.py

--- adivino.py
+++ adivino.py
@@ -1,7 +1,7 @@
 import random
 numero_aleatorio = random.randrange(100)
 gane = False
-print("Tenés 3 intentos para adivinar un entre 0 y 99")
+print("Tenés 5 intentos para adivinar un entre 0 y 99")
 intento = 1
 while intento < 6 and not gane:
     numero_ingresado = int(input('Ingresa tu número: '))
